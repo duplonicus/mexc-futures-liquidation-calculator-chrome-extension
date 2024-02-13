e00b74e093c6a6ffd32a1a4b4757bd281124b8bb
Jake Caza <jake.caza@gmail.com>
2024-02-12 21:23:37 -0500

    workingish

--- content_script.js
+++ content_script.js
@@ -1,45 +1,110 @@
+/* 
+    - Support linear and cross margin
+    - Support risk limit tiers, current support is tier 1
+    - Calculation: https://www.mexc.com/support/articles/360044646391
+ */
+
+let failCount = 0;
+let priceElement;
+let leverageElement;
+let i = 0;
+let liqPrices;
+
+// Create observer object
+let observer = new MutationObserver(getLiq);
+
+// Create a new div element with an id of "liqPrices"
+liqPrices = document.createElement("div");
+liqPrices.id = "liqPrices";
+document.body.appendChild(liqPrices);
+liqPrices.style.position = "fixed";
+liqPrices.style.bottom = "0";
+liqPrices.style.right = "0";
+liqPrices.style.backgroundColor = "#01081e";
+liqPrices.style.color = "#fafafa";
+liqPrices.style.padding = "15px";
+liqPrices.style.fontSize = "14px";
+liqPrices.style.setProperty("color", "#fafafa", "important"); // Force color to override other styles
+liqPrices.style.setProperty("background-color", "#01081e", "important"); // Force background color to override other styles
+liqPrices.textContent = 'Loading liquidation prices...'; // Add static content
+
+
+console.log('waiting for page to load...');
+
 const waitForPage = setTimeout(() => {
-  console.log('############ Testing ############');
-  // Get the orderbox node
-  orderBoxElement = document.evaluate('//*[@id="mexc-web-inspection-futures-exchange-orderForm"]/div[2]/div[1]/section/div[2]/span', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+
+  // Create a new div element with an id of "liqPrices"
+  /* const liqPrices = document.createElement("div");
+  liqPrices.id = "liqPrices"; */
+
+  // Style the div to make it visible and positioned correctly
+  /* liqPrices.style.position = "fixed";
+  liqPrices.style.bottom = "0";
+  liqPrices.style.right = "0";
+  liqPrices.style.backgroundColor = "#01081e";
+  liqPrices.style.color = "#fafafa";
+  liqPrices.style.padding = "15px";
+  liqPrices.style.fontSize = "14px"; */
+
+  // Append the new element to the body of the page
+  /* document.body.appendChild(liqPrices);
+  console.log("liqPrices element created"); */
+
+  // Style it to look like the Kucoin dark theme
+  /* liqPrices.style.color = "#fafafa";
+  liqPrices.style.backgroundColor = "#01081e"; */
+
+  // Move it to the bottom right
+/*   liqPrices.style.textAlign = "right";
+  liqPrices.style.paddingRight = "15px"; */
+
+  // Call newTicker to start the process
+  newTicker();
+
+}, 10000);
+
+// Runs at start and if the ticker changes
+function newTicker() {
+  
+  // Get the leverage node
+  leverageElement = document.evaluate('//*[@id="mexc-web-inspection-futures-exchange-orderForm"]/div[2]/div[1]/section/div[2]/span', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
   // Get the last price node
   priceElement = document.evaluate('//*[@id="mexc-web-inspection-futures-exchange-orderbook"]/div[2]/div[2]/div[2]/span/div/h3/span[1]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
   // Return if either of them are not found
-  if (!orderBoxElement || !priceElement) {
-    console.log('Extension failed to load!')
-    return;
+  if (!leverageElement || !priceElement) {
+    console.log('Price and/or Leverage elements not found!');
+    // Try again in 3 seconds
+    setTimeout(() => newTicker(), 3000);
+    // Increment the fail count
+    failCount += 1;
+    console.log('Fail: ', failCount);
+    // If we fail 5 times, stop trying
+    if (failCount > 5) {
+      console.log('Failed to find elements 5 times, stopping');
+      observer.disconnect();
+      return;
+    }
   }
-
-  // Mutation Observers to detect changes to the slider and price
-  orderBoxObserver = new MutationObserver(getLiq);
-  orderBoxObserver.observe(orderBoxElement, { characterData: true, attributes: true, childList: true, subtree: true });
-  priceObserver = new MutationObserver(getLiq);
-  priceObserver.observe(priceElement, { characterData: true, attributes: false, childList: false, subtree: true });
-
-  // Create a new div element with an id of "liqPrices"
-  const liqPrices = document.createElement("div");
-  liqPrices.id = "liqPrices";
-
-  // Append the new element to the body of the page
-  document.body.appendChild(liqPrices);
-  console.log("liqPrices element created ##############")
-
-  // Style it to look like the Kucoin dark theme
-  liqPrices.style.color = "#fafafa";
-  liqPrices.style.backgroundColor = "#01081e";
-
-  // Move it to the bottom right
-  liqPrices.style.textAlign = "right";
-  liqPrices.style.paddingRight = "15px";
-
-  // Run once to get the initial values
-  getLiq();
-
-}, 15000);
-
-let i = 1;
+  // re-attach the observer
+  observer.disconnect();
+  observer.observe(leverageElement, { characterData: true, attributes: true, childList: true, subtree: true });
+  observer.observe(priceElement, { characterData: true, attributes: false, childList: false, subtree: true });
+  console.log('Mutation Observers started');
+  //console.log(observer);
+}
 
 function getLiq() {
+
+  // Check if the priceElement node is still valid and call newTicker if it's not
+  //console.log('getLiq:priceElement ', priceElement);
+  let newPriceElement = document.evaluate('//*[@id="mexc-web-inspection-futures-exchange-orderbook"]/div[2]/div[2]/div[2]/span/div/h3/span[1]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+  if (newPriceElement != priceElement) {
+    priceElement = newPriceElement;
+    console.log('Ticker changed');
+    //console.log('New priceElement: ', newPriceElement);
+    setTimeout(() => newTicker(), 3000);
+    return; 
+  }
 
   // Get the value of the leverage slider
   let sliderVal = parseInt(document.evaluate('//*[@id="mexc-web-inspection-futures-exchange-orderForm"]/div[2]/div[1]/section/div[2]/span', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent, 10);
@@ -47,9 +112,11 @@ function getLiq() {
   // Get the value of the last price
   let lastPrice = document.evaluate('//*[@id="mexc-web-inspection-futures-exchange-orderbook"]/div[2]/div[2]/div[2]/span/div/h3/span[1]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent.trim();
 
-
   // Get the number of digits after the decimal
   let digits = lastPrice.toString().split(".")[1].length;
+
+  // Replace commas with nothing and convert to a float
+  lastPrice = lastPrice.replace(/,/g, '');
   lastPrice = parseFloat(lastPrice);
 
   // Calculate the liquidation prices
@@ -63,15 +130,16 @@ function getLiq() {
 
   // Debug
   console.log('############ Testing ############', i);
-  console.log('Slider: ', sliderVal);
+  console.log('Leverage: ', sliderVal);
   console.log('Last Price: ', lastPrice);
   console.log('Liquidation %: ', parseFloat(liqPcnt.toFixed(2)));
   console.log('Liquidation Distance: ', liqPriceDist);
   console.log('Long Liquidation: ', longLiqPrice);
   console.log('Short Liquidation: ', shortLiqPrice);
   console.log('Digits: ', digits);
-  console.log(orderBoxElement, priceElement);
+  //console.log(leverageElement, priceElement);
 
   i += 1;
 
 }
+
